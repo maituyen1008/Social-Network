7cb893c529e33594afbbbee7849136dc2e89a76a
maituyen1008 <maituyen100897@gmail.com>
2019-04-08 21:38:35 +0700

    create route login and register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/register', function(request, response) {
+  response.render('users/register');
+})
+
+router.get('/login', function(request, response, next) {
+  response.render('users/login');
 });
+
+router.post('/login', function(request, response) {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/user/login',
+  })
+})
 
 module.exports = router;
