777022a01f0b09d8e69bd0fcc69964d094daa7a5
maituyen1008 <maituyen100897@gmail.com>
2019-04-22 17:34:51 +0700

    add modules multer, cloudinary anh update login page

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/User');
+var multer = require("multer");
+var cloudinary = require("cloudinary");
 
 /* GET users listing. */
+
+// Multer setup
+const storage = multer.diskStorage({
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + file.originalname);
+    }
+});
+
+const imageFilter = (req, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/i)) {
+        return callback(new Error("Only image files are allowed!"), false);
+    }
+    callback(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFilter });
+
+// Cloudinary setup
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const isLoggedIn = (request, response, next) => {
     if (request.isAuthenticated()) {
@@ -14,23 +39,31 @@ const isLoggedIn = (request, response, next) => {
 }
 
 //Get register get route
-router.get('/user/register', function(request, response) {
-    response.render('users/register');
+router.get('/user/login', function(request, response) {
+    response.render('users/login');
 })
 
 //Get register post route
-router.post('/user/register', function(request, response) {
-    if (request.body.email 
-        && request.body.firstname
-        && request.body.lastname
-        && request.body.password 
+router.post('/user/register', upload.single("image"), function(request, response) {
+    if ( request.body.username
+        && request.body.firstName
+        && request.body.lastName 
+        && request.body.password
     ) {
         var newUser = new User ({
-            username : request.body.email,
+            username : request.body.username,
             firstName : request.body.firstname,
             lastName : request.body.lastname,
         })
-        createUser (newUser, request.body.password, request, response);
+        if (request.file) {
+            cloudinary.uploader.upload(request.file.path, result => {
+                newUser.profile = result.secure_url;
+                return createUser(newUser, request.body.password, request, response);
+            });
+        } else {
+            newUser.profile = process.env.DEFAULT_PROFILE_PIC;
+            return createUser(newUser, request.body.password, request, response);
+        }    
     }
 })
 
@@ -54,8 +87,8 @@ router.get('/user/login', function(request, response, next) {
 });
 
 router.post('/user/login', passport.authenticate('local', {
-        successRedirect: '/user/register',
-        failureRedirect: '/',
+        successRedirect: '/',
+        failureRedirect: '/user/login',
     }),
     (request, response) => {}
 )
